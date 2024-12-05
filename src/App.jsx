aa94f292a8f8e1efffdc125d8f46abda754548f0
ISinFor7 <yahia.berrouiguet.lilo@gmail.com>
2024-12-05 22:05:03 +0100

    js functions + database

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { points } from './Fonctions.js'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + points())}>
           count is {count}
         </button>
         <p>
