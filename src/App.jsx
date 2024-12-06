9050b7a438cbb7e0b84bc6001a689ac9bb08ca78
akaSHI26 <mangakpop2000@gmail.com>
2024-12-06 03:59:16 +0100

    images

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      
+      
       <div className="card">
         <img id='click_button' src='/src/assets/Photo/CannePeche/canneBasique.png' onClick={() => setCount((count) => count + 1)} style={{"pointer-events": "all"}} />
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          {count} Money
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <h1> Missions & Achievements  </h1>
       <div className="card">
@@ -57,9 +47,7 @@ function App() {
           </div>
         </div>
       )}
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
