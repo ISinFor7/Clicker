08f751c952c9957b9c12c85ab75b8263c1ca7f79
SergeantRyan <rayen.laarif4@gmail.com>
2024-12-06 02:37:24 +0100

    inventory

--- src/Fonctions.ts
+++ src/Fonctions.ts
@@ -38,3 +38,20 @@ function equip(item) {}
 function buy(augment) {}
 
 function gacha() {}
+
+function inventory() {
+  return (
+      <div style={{padding: "20px", fontFamily: "Arial, sans-sherif"}}>
+          <h1>Inventaire</h1>
+          {inventory.length===0 ? (
+              <p>Aucun objet dans l'inventaire.</p>
+          ) : (
+              <ul>
+                  {inventory.map((item, index) => (
+                      <li key={index}>{item}</li>
+                  ))}
+              </ul>
+          )}
+      </div>
+  );
+}
