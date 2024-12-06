a2a62defce11b48cba2ebd82ba291ed1ff6a4c9b
ISinFor7 <yahia.berrouiguet.lilo@gmail.com>
2024-12-06 03:34:26 +0100

    changement joueurs

--- src/Fonctions.ts
+++ src/Fonctions.ts
@@ -1,11 +1,6 @@
 export default { points, equip, buy, gacha };
 
 //création des énumérations
-enum nom_cap {
-  cannePrenium,
-  Arpon,
-}
-
 enum fish {
   Baleine,
   poissonBasique,
@@ -24,12 +19,31 @@ enum ocean {
 
 enum stat {
   poissonSup,
+  rareteSup,
+  multPeche,
 }
 
 //création des cannes à pêche
+
+type cap = {
+  nom: string;
+  stat: stat;
+  percent: number;
+};
+
 const canneBasique: cap = {
   nom: "canneBasique",
   stat: stat.poissonSup,
+  percent: 5,
+};
+const cannePremium: cap = {
+  nom: "cannePremium",
+  stat: stat.rareteSup,
+  percent: 10,
+};
+const Arpon: cap = {
+  nom: "Arpon",
+  stat: stat.multPeche,
   percent: 5,
 };
 
@@ -38,12 +52,9 @@ const j1: joueur = {
   nb_coraux: 0,
   nb_dechets: 0,
   nb_poissons: 0,
-};
-
-type cap = {
-  nom: string;
-  stat: stat;
-  percent: number;
+  poissonSup: 1,
+  rareteSup: 1,
+  multPeche: 1,
 };
 
 type augment = {
@@ -60,14 +71,20 @@ type joueur = {
   nb_poissons: number;
   nb_coraux: number;
   nb_dechets: number;
+  poissonSup: number;
+  rareteSup: number;
+  multPeche: number;
 };
 
 function points() {
-  let chiffre = Math.random() * 5;
+  let chiffre = Math.random() * 5 * stat.poissonSup;
   if (chiffre > 3) {
     return;
+  } else if (chiffre > 2) {
+    return;
+  } else {
+    return 1;
   }
-  return 1;
 }
 
 function equip(item: cap) {}
